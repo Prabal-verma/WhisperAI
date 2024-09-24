f58fcdbefc7048dc1c502e0ed99ae68ee1918459
Prabal Verma <prableverma@gmail.com>
2024-09-24 18:57:10 +0530

    credential provider added

--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -3,6 +3,7 @@ import { signIn } from "@/auth"
  
 export default function SignIn() {
   return (
+    <>
     <form
       action={async () => {
         "use server"
@@ -11,5 +12,22 @@ export default function SignIn() {
     >
       <button type="submit">Signin with Google</button>
     </form>
+    <form
+      action={async (formData) => {
+        "use server"
+        await signIn("credentials", formData)
+      }}
+    >
+      <label>
+        Email
+        <input name="email" type="email" />
+      </label>
+      <label>
+        Password
+        <input name="password" type="password" />
+      </label>
+      <button>Sign In</button>
+    </form>
+    </>
   )
 } 
